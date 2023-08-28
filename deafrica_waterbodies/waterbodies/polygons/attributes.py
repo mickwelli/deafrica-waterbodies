b1c2b3f9568c2ac53b02499b72a1deee065fa5e9
vikineema <victoria@kartoza.com>
2023-08-28 13:09:44 +0300

    Update get_attributes function

--- deafrica_waterbodies/waterbodies/polygons/attributes.py
+++ deafrica_waterbodies/waterbodies/polygons/attributes.py
@@ -16,11 +16,14 @@ def add_attributes(waterbodies_gdf, timeseries_output_bucket, timeseries_product
     waterbodies_gdf_6933 = waterbodies_gdf.to_crs("EPSG:6933")
 
     # Perimeter
-    waterbodies_gdf_6933["perim_m"] = waterbodies_gdf_6933.geometry.length.round(decimals=4)
+    waterbodies_gdf_6933["perim_m"] = waterbodies_gdf_6933.geometry.length
     # Area
-    waterbodies_gdf_6933["area_m2"] = waterbodies_gdf_6933.geometry.area.round(decimals=4)
+    waterbodies_gdf_6933["area_m2"] = waterbodies_gdf_6933.geometry.area
     # Timeseries url
     waterbodies_gdf_6933["timeseries"] = waterbodies_gdf_6933.apply(lambda row: get_timeseries_url_from_uid(row["UID"], timeseries_output_bucket, timeseries_product_version), axis=1)
+
+    # Round the values in the perimeter and area columns to the specified number of decimal places.
+    waterbodies_gdf_6933 = waterbodies_gdf_6933.round({"perim_m": 4, "area_m2": 4})
 
     # Project to epsg:4326
     waterbodies_gdf_4326 = waterbodies_gdf_6933.to_crs("EPSG:4326")
