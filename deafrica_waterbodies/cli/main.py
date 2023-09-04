cd79dfcd87e9b2a9a6634db64c63cf1e209e3a4b
vikineema <victoria@kartoza.com>
2023-09-04 13:23:44 +0300

    Add generate timeseries command

--- deafrica_waterbodies/cli/main.py
+++ deafrica_waterbodies/cli/main.py
@@ -3,7 +3,7 @@ import click
 import deafrica_waterbodies.__version__
 
 from deafrica_waterbodies.cli.generate_waterbodies import generate_waterbodies
-
+from deafrica_waterbodies.cli.generate_timeseries import generate_timeseries
 
 @click.version_option(package_name="deafrica_waterbodies", version=deafrica_waterbodies.__version__)
 @click.group(help="Run deafrica-waterbodies.")
@@ -12,3 +12,4 @@ def main():
 
 
 main.add_command(generate_waterbodies)
+main.add_command(generate_timeseries)
