373815e310d9e8391c48f979a36a4b9df6a2af55
vikineema <victoria@kartoza.com>
2023-10-02 16:32:39 +0300

    Remove default s3 region from environment variables

--- index_tiles.sh
+++ index_tiles.sh
@@ -2,7 +2,7 @@
 
 # Add the wofs_ls_summary_alltime datasets.
 s3-to-dc "s3://deafrica-services/wofs_ls_summary_alltime/1-0-0/x204/y048/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls_summary_alltime'
-s3-to-dc "s3://deafrica-services/wofwofs_ls_summary_alltimes_ls/1-0-0/x204/y049/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls_summary_alltime'
+s3-to-dc "s3://deafrica-services/wofs_ls_summary_alltime/1-0-0/x204/y049/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls_summary_alltime'
 
 s3-to-dc "s3://deafrica-services/wofs_ls_summary_alltime/1-0-0/x205/y048/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls_summary_alltime'
 s3-to-dc "s3://deafrica-services/wofs_ls_summary_alltime/1-0-0/x205/y049/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls_summary_alltime'
