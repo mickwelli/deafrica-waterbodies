0054ddb931e84dc01fa6b0cb21fa55242a5c5686
vikineema <victoria@kartoza.com>
2023-10-03 20:44:01 +0300

    Add push-to-sqs-queue command

--- deafrica_waterbodies/cli/main.py
+++ deafrica_waterbodies/cli/main.py
@@ -2,6 +2,7 @@ import click
 
 import deafrica_waterbodies.__version__
 from deafrica_waterbodies.cli.get_dataset_ids import get_dataset_ids
+from deafrica_waterbodies.cli.push_to_sqs_queue import push_to_sqs_queue
 from deafrica_waterbodies.cli.generate_timeseries import generate_timeseries
 from deafrica_waterbodies.cli.generate_waterbodies_test import generate_waterbodies
 
@@ -13,5 +14,6 @@ def main():
 
 
 main.add_command(get_dataset_ids)
+main.add_command(push_to_sqs_queue)
 main.add_command(generate_waterbodies)
 main.add_command(generate_timeseries)
--- deafrica_waterbodies/cli/push_to_sqs_queue.py
+++ deafrica_waterbodies/cli/push_to_sqs_queue.py
@@ -1,5 +1,5 @@
 import logging
-
+import os
 import boto3
 import click
 
@@ -9,10 +9,10 @@ from deafrica_waterbodies.cli.logs import logging_setup
 
 @click.command("push-to-sqs-queue", no_args_is_help=True)
 @click.option(
-    "--text-file-path",
+    "--output-directory",
     type=click.Path(),
     required=True,
-    help="REQUIRED. Path to text file containing the dataset ids to push to queue.",
+    help="Directory containing the dataset ids text file.",
 )
 @click.option("--queue-name", required=True, help="REQUIRED. Queue name to push to.")
 @click.option(
@@ -21,7 +21,7 @@ from deafrica_waterbodies.cli.logs import logging_setup
     help="Maximum number of times to retry sending/receiving messages to/from a SQS queue.",
 )
 @click.option("-v", "--verbose", count=True)
-def push_to_sqs_queue(text_file_path, queue_name, max_retries, verbose):
+def push_to_sqs_queue(output_directory, queue_name, max_retries, verbose):
     """
     Push dataset ids from the lines of a text file to a SQS queue.
     """
@@ -30,6 +30,9 @@ def push_to_sqs_queue(text_file_path, queue_name, max_retries, verbose):
 
     # Create an sqs client.
     sqs_client = boto3.client("sqs")
+
+    # Dataset ids text file.
+    text_file_path = os.path.join(output_directory, "dataset_ids.txt")
 
     failed_to_push = deafrica_waterbodies.queues.push_dataset_ids_to_queue_from_txt(
         text_file_path=text_file_path,
