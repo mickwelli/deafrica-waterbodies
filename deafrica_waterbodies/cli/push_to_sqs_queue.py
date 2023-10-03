adf5bd7dca588f5f244158a4fda3ade6bf0f1f16
vikineema <victoria@kartoza.com>
2023-10-03 20:54:47 +0300

    Rename queue_name to dataset_ids_queue

--- deafrica_waterbodies/cli/push_to_sqs_queue.py
+++ deafrica_waterbodies/cli/push_to_sqs_queue.py
@@ -14,14 +14,14 @@ from deafrica_waterbodies.cli.logs import logging_setup
     required=True,
     help="Directory containing the dataset ids text file.",
 )
-@click.option("--queue-name", required=True, help="REQUIRED. Queue name to push to.")
+@click.option("--dataset-ids-queue", required=True, help="Name of the queue to push the dataset ids to.")
 @click.option(
     "--max-retries",
     default=10,
     help="Maximum number of times to retry sending/receiving messages to/from a SQS queue.",
 )
 @click.option("-v", "--verbose", count=True)
-def push_to_sqs_queue(output_directory, queue_name, max_retries, verbose):
+def push_to_sqs_queue(output_directory, dataset_ids_queue, max_retries, verbose):
     """
     Push dataset ids from the lines of a text file to a SQS queue.
     """
@@ -36,14 +36,14 @@ def push_to_sqs_queue(output_directory, queue_name, max_retries, verbose):
 
     failed_to_push = deafrica_waterbodies.queues.push_dataset_ids_to_queue_from_txt(
         text_file_path=text_file_path,
-        queue_name=queue_name,
+        queue_name=dataset_ids_queue,
         max_retries=max_retries,
         sqs_client=sqs_client,
     )
 
     if failed_to_push:
         # Push the failed dataset ids to the deadletter queue.
-        dead_letter_queue_name = f"{queue_name}-deadletter"
+        dead_letter_queue_name = f"{dataset_ids_queue}-deadletter"
         dead_letter_queue_url = deafrica_waterbodies.queues.get_queue_url(
             queue_name=dead_letter_queue_name, sqs_client=sqs_client
         )
