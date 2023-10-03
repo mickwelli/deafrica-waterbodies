c5c28424d43627856b4ef4f06f38b9289e4bc787
vikineema <victoria@kartoza.com>
2023-10-03 22:05:24 +0300

    Add command to generate waterbodies from dataset ids in text file

--- deafrica_waterbodies/cli/main.py
+++ deafrica_waterbodies/cli/main.py
@@ -4,6 +4,7 @@ import deafrica_waterbodies.__version__
 from deafrica_waterbodies.cli.get_dataset_ids import get_dataset_ids
 from deafrica_waterbodies.cli.push_to_sqs_queue import push_to_sqs_queue
 from deafrica_waterbodies.cli.run_from_sqs_queue import run_from_sqs_queue
+from deafrica_waterbodies.cli.run_from_txt import run_from_txt
 
 
 @click.version_option(package_name="deafrica_waterbodies", version=deafrica_waterbodies.__version__)
@@ -15,3 +16,4 @@ def main():
 main.add_command(get_dataset_ids)
 main.add_command(push_to_sqs_queue)
 main.add_command(run_from_sqs_queue)
+main.add_command(run_from_txt)
--- deafrica_waterbodies/cli/run_from_txt.py
+++ deafrica_waterbodies/cli/run_from_txt.py
@@ -10,7 +10,7 @@ import deafrica_waterbodies.make_polygons
 from deafrica_waterbodies.cli.logs import logging_setup
 
 
-@click.command("generate-waterbodies-from-thresholds", no_args_is_help=True)
+@click.command("run-from-txt", no_args_is_help=True)
 @click.option("-v", "--verbose", count=True)
 @click.option(
     "--primary-threshold",
@@ -38,44 +38,63 @@ from deafrica_waterbodies.cli.logs import logging_setup
     type=click.Path(),
     help="Directory to write the waterbody polygons to.",
 )
-def generate_waterbodies_from_thresholds(
+@click.option(
+    "--dataset-ids-text-file",
+    type=click.Path(),
+    required=True,
+    help="Path to dataset ids text file.",
+)
+@click.option(
+    "--overwrite/--no-overwrite",
+    default=False,
+    help="Rerun scenes that have already been processed.",
+)
+def run_from_txt(
     verbose,
     primary_threshold,
     secondary_threshold,
     minimum_valid_observations,
     output_directory,
+    dataset_ids_text_file,
+    overwrite,
 ):
+    """
+    Generate waterbody polygons from WOfS All Time Summary scenes whose ids are
+    in a text file.
+    """
     # Set up logger.
     logging_setup(verbose=verbose)
     _log = logging.getLogger(__name__)
 
     # Support pathlib paths.
     output_directory = str(output_directory)
+    dataset_ids_text_file = str(dataset_ids_text_file)
 
     dask_chunks = {"x": 3200, "y": 3200, "time": 1}
     resolution = (-30, 30)
     output_crs = "EPSG:6933"
 
-    # Instanstiate the filesystem to use.
-    if deafrica_waterbodies.io.check_if_s3_uri(output_directory):
-        fs = fsspec.filesystem("s3")
-    else:
-        fs = fsspec.filesystem("file")
-
-    # Check if the dataset ids text file exists in the output directory.
-    dataset_ids_txt = os.path.join(output_directory, "dataset_ids.txt")
-
-    if not deafrica_waterbodies.io.check_file_exists(dataset_ids_txt):
-        _log.error(f"Could not find text file {dataset_ids_txt}!")
-        raise FileNotFoundError(f"Could not find text file {dataset_ids_txt}!")
-
     # Read the dataset ids from the text file.
-    with fs.open(dataset_ids_txt, "r") as file:
-        lines = file.readlines()
-        dataset_ids = [line.strip() for line in lines]
+    if not deafrica_waterbodies.io.check_file_exists(dataset_ids_text_file):
+        _log.error(f"Could not find text file {dataset_ids_text_file}!")
+        raise FileNotFoundError(f"Could not find text file {dataset_ids_text_file}!")
+    else:
+        if deafrica_waterbodies.io.check_if_s3_uri(dataset_ids_text_file):
+            fs = fsspec.filesystem("s3")
+        else:
+            fs = fsspec.filesystem("file")
+        with fs.open(dataset_ids_text_file, "r") as file:
+            lines = file.readlines()
+            dataset_ids = [line.strip() for line in lines]
 
     # Directory to write generated waterbody polygons to.
     polygons_from_thresholds_dir = os.path.join(output_directory, "polygons_from_thresholds")
+
+    # Instanstiate the filesystem to use.
+    if deafrica_waterbodies.io.check_if_s3_uri(polygons_from_thresholds_dir):
+        fs = fsspec.filesystem("s3")
+    else:
+        fs = fsspec.filesystem("file")
 
     # Check if the directory exists. If it does not, create it.
     if not deafrica_waterbodies.io.check_dir_exists(polygons_from_thresholds_dir):
