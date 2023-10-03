30fa5fb887d1289945eb12e796308d6d12cd54bd
vikineema <victoria@kartoza.com>
2023-10-03 16:40:06 +0300

    Update get_dataset_ids to use output directory

--- deafrica_waterbodies/cli/get_dataset_ids.py
+++ deafrica_waterbodies/cli/get_dataset_ids.py
@@ -1,14 +1,13 @@
 import logging
 import os
-import urllib
 
 import click
 import fsspec
 import geopandas as gpd
 
 from deafrica_waterbodies.cli.logs import logging_setup
-from deafrica_waterbodies.io import check_if_s3_uri
-from deafrica_waterbodies.make_polygons import get_datasets_ids
+import deafrica_waterbodies.io
+import deafrica_waterbodies.make_polygons
 
 
 @click.command("get-dataset-ids", no_args_is_help=True)
@@ -23,19 +22,23 @@ from deafrica_waterbodies.make_polygons import get_datasets_ids
     "--num-workers", type=int, help="Number of worker processes to use when filtering datasets."
 )
 @click.option(
-    "--output-file-path",
+    "--output-directory",
     type=click.Path(),
-    help="File URI or S3 URI of the text file to write the dataset ids to.",
+    help="Directory to write the dataset ids text file to.",
 )
 def get_dataset_ids(
     verbose,
     aoi_vector_file,
     num_workers,
-    output_file_path,
+    output_directory,
 ):
     # Set up logger.
     logging_setup(verbose=verbose)
     _log = logging.getLogger(__name__)
+
+    # Support pathlib Paths.
+    aoi_vector_file = str(aoi_vector_file)
+    output_directory = str(output_directory)
 
     # Load the area of interest as a GeoDataFrame.
     if aoi_vector_file is not None:
@@ -49,20 +52,21 @@ def get_dataset_ids(
 
     # Get the WOfS All Time Summary scene ids for the scenes whose extent
     # intersects with the area of interest.
-    dataset_ids = get_datasets_ids(aoi_gdf=aoi_gdf, num_workers=num_workers)
+    dataset_ids = deafrica_waterbodies.make_polygons.get_datasets_ids(aoi_gdf=aoi_gdf, num_workers=num_workers)
 
-    # Write the dataset ids to a text file.
-    if check_if_s3_uri(output_file_path):
+    # Instanstiate the filesystem to use.
+    if deafrica_waterbodies.io.check_if_s3_uri(output_directory):
         fs = fsspec.filesystem("s3")
     else:
         fs = fsspec.filesystem("file")
-        parsed_output_fp = urllib.parse.urlparse(output_file_path).path
-        absolute_output_fp = os.path.abspath(parsed_output_fp)
-        path_head, path_tail = os.path.split(absolute_output_fp)
-        if path_head:
-            if not fs.exists(path_head):
-                fs.mkdirs(path_head, exist_ok=True)
-                _log.info(f"Local folder {path_head} created.")
+
+    # Check if the output directory exists. If it does not, create it.
+    if not deafrica_waterbodies.io.check_dir_exists(output_directory):
+        fs.mkdirs(output_directory, exist_ok=True)
+        _log.info(f"Created directory {output_directory}")
+
+    # Write the dataset ids to a text file.
+    output_file_path = os.path.join(output_directory, "dataset_ids.txt")
 
     with fs.open(output_file_path, "w") as file:
         for dataset_id in dataset_ids:
