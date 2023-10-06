03c3dcffb0d34dd698e914a641fca8a9dd18c16e
vikineema <victoria@kartoza.com>
2023-10-06 22:08:15 +0300

    Add filter_tiles function

--- deafrica_waterbodies/tiling.py
+++ deafrica_waterbodies/tiling.py
@@ -1,5 +1,9 @@
+import multiprocessing
+from functools import partial
+
 import datacube
 import geopandas as gpd
+import tqdm
 from odc.dscache.tools.tiling import parse_gridspec_with_name
 
 
@@ -36,3 +40,39 @@ def check_tile_intersects_polygons(
             return ()
     else:
         return tile_id
+
+
+def filter_tiles(
+    tiles: dict[tuple[int, int], datacube.api.grid_workflow.Tile],
+    polygons_gdf: gpd.GeoDataFrame | None,
+    num_workers: int = 8,
+) -> list[tuple[int, int]]:
+    """
+    Filter out tiles that do not intersect with a set of polygons, using a
+    multi-process approach to run the `check_tile_intersects_polygons` function.
+
+    Parameters
+    ----------
+    tiles: dict[tuple[int,int], datacube.api.grid_workflow.Tile]
+        A dictionary of tiles.
+    polygons_gdf : gpd.GeoDataFrame | None
+        A set of polygons in a GeoDataFrame
+    num_workers : int, optional
+        Number of worker processes to use during multi-processing, by default 8
+
+    Returns
+    -------
+    list[tuple[int, int]]
+        A list of the filtered tiles ids.
+    """
+    with multiprocessing.Pool(processes=num_workers) as pool:
+        filtered_tile_ids_ = list(
+            tqdm.tqdm(
+                pool.imap(partial(check_tile_intersects_polygons, polygons_gdf), tiles.items())
+            )
+        )
+
+    # Remove empty tuples.
+    filtered_tile_ids = [item for item in filtered_tile_ids_ if item]
+
+    return filtered_tile_ids
