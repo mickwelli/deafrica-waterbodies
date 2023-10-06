910b3159b6e40341bc121692b4579f58acb99dd5
vikineema <victoria@kartoza.com>
2023-10-06 21:22:46 +0300

    Add function to mask  hydroSHEDS land mask into a boolean mask

--- deafrica_waterbodies/filters.py
+++ deafrica_waterbodies/filters.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+import xarray as xr
 
 _log = logging.getLogger(__name__)
 
@@ -640,3 +641,12 @@ def filter_waterbodies(
     )
 
     return filtered_polygons
+
+
+def filter_hydrosheds_land_mask(hydrosheds_land_mask: xr.DataArray) -> xr.DataArray:
+    """
+    Function to filter the HydroSHEDs Land Mask into a boolean mask.
+    """
+    # Indicator values: 1 = land, 2 = ocean sink, 3 = inland sink, 255 is no data.
+    boolean_mask = (hydrosheds_land_mask != 255) & (hydrosheds_land_mask != 2)
+    return boolean_mask
